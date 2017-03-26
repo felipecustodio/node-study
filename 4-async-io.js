096f1fab6ad20758ecf1c021d9f40c785410e743
Felipe Scrochio Custódio <felipe.crochi@gmail.com>
2017-03-26 16:48:17 -0300

    Style fix

--- 4-async-io.js
+++ 4-async-io.js
@@ -1,19 +1,19 @@
-var fs = require('fs')
-var lines = 0
+var fs = require('fs');
+var lines = 0;
 
 function countLines(callback) {
     fs.readFile(process.argv[2], function doneReading(err, fileContents) {
-    var text = String(fileContents)
+    var text = String(fileContents);
     for (var i = 0; i < text.length; i++) {
             if (text[i] == '\n')
-                lines++
+                lines++;
     }
-    callback()
-  })
+    callback();
+  });
 }
 
 function logLines() {
-  console.log(lines)
+  console.log(lines);
 }
 
-countLines(logLines)
+countLines(logLines);
